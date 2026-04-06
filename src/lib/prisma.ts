6f2ede60aa7c0c7e9890afdcef43386f831b3d04
Jonathan Moya Moreno <djgrovezone@gmail.com>
2026-04-06 17:04:09 +0200

    feat: initialize Prisma client with custom pg adapter and add git credentials file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,17 +4,25 @@ if (process.env.NODE_ENV === "development") {
 }
 import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const { Pool } = pg;
 const rawConnectionString = process.env.DATABASE_URL;
-const connectionString = rawConnectionString?.replace("sslmode=require", "sslmode=no-verify");
+const connectionString = rawConnectionString?.replace(
+  "sslmode=require",
+  "sslmode=no-verify",
+);
 
 if (!connectionString) {
   console.warn("WARNING [PRISMA_INIT]: DATABASE_URL is not defined.");
 } else {
-  console.log("LOG [PRISMA_INIT]: Initializing prisma with direct DATABASE_URL.");
-  console.log("LOG [PRISMA_INIT]: NODE_TLS_REJECT_UNAUTHORIZED =", process.env.NODE_TLS_REJECT_UNAUTHORIZED);
+  console.log(
+    "LOG [PRISMA_INIT]: Initializing prisma with direct DATABASE_URL.",
+  );
+  console.log(
+    "LOG [PRISMA_INIT]: NODE_TLS_REJECT_UNAUTHORIZED =",
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED,
+  );
 }
 
 const pool = new Pool({
